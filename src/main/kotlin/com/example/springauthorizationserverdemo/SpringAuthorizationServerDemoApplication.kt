183ee02aaeabc68452d4277d4cc047d846cf910a
Hao Wang <hao.wang@flexport.com>
2023-09-02 18:02:00 +0800

    init

---
+++ src/main/kotlin/com/example/springauthorizationserverdemo/SpringAuthorizationServerDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springauthorizationserverdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAuthorizationServerDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringAuthorizationServerDemoApplication>(*args)
+}
